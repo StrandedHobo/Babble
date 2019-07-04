c9a85f0001a6f844273b58c63996eb6ebc18f9aa
Dymaxx <45269003+Dymaxx@users.noreply.github.com>
2019-07-04 09:29:44 +0200

    Database

---
+++ babbel/db/dbconfig.php
@@ -0,0 +1,6 @@
+<?php
+$dbhost = "51.68.122.18";
+$dbuser = "overderheinheiskraan";
+$dbpassword = "Tw:6+2uRw<)@k";
+$dbname = "overderhein";
+?>
---
+++ babbel/db/dbopen.php
@@ -0,0 +1,8 @@
+<?php
+  $dblink = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname)
+    or die("ERROR! More info:".mysqli_connect_error());
+
+    set_time_limit(60)
+
+
+ ?>
