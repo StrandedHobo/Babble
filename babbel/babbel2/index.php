7d1c931c52a9c473ae8ce9c510bce198a54dfb4e
Dymaxx <45269003+Dymaxx@users.noreply.github.com>
2019-07-04 12:22:23 +0200

    bas

---
+++ babbel/babbel2/index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Inlogscherm</title>
+  <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+ <div id="login">
+  <h2>Babbel Inlogscherm</h2>
+  <form action="insertintodb.php" method="post">
+   <label>Email :</label>
+   <input id="name" name="name" placeholder="name" type="text">
+   <label>email :</label>
+   <input id="email" name="email" placeholder="email" type="text">
+   <label>Wachtwoord :</label>
+   <input id="password" name="password" placeholder="**********" type="password"><br><br>
+   <input name="submit" type="submit" value=" Login ">
+
+  </form>
+ </div>
+</body>
+</html>
---
+++ babbel/babbel2/overview.php
@@ -0,0 +1,24 @@
+<? php
+
+<?php
+include("db/dbconfig.php");
+include("db/dbopen.php");
+
+
+
+$query = "SELECT name FROM user ";
+$result = mysqli_query($con,$query);
+
+while($row = mysqli_fetch_assoc($result)){
+    $stringTest = $row['balance'];
+    echo $stringTest;
+}
+
+
+
+ include("db/dbclose.php");
+
+
+?>
+
+?>
